8764a3391494d0122cab63e6b1182e0894d083f8
isantos <isantos@tash-linux-note>
2018-12-30 23:49:34 -0200

    Criando o projeto

---
+++ travelmanager/src/main/java/com/transport/travelmanager/TravelmanagerApplication.java
@@ -0,0 +1,14 @@
+package com.transport.travelmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelmanagerApplication.class, args);
+	}
+
+}
+
---
+++ travelmanager/src/test/java/com/transport/travelmanager/TravelmanagerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.transport.travelmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TravelmanagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
